cac65d8af488ba7976bda6ecf618736dfa2854a1
Nick Ryder <nick.ryder@openai.com>
2019-12-21 13:27:38 -0800

    working on validate

--- PoEItem/clipboard_parser/mod_line_to_stat.py
+++ PoEItem/clipboard_parser/mod_line_to_stat.py
@@ -1,5 +1,9 @@
 from RePoE import stat_translations
 from RePoE import mods
+from tqdm import tqdm
+
+import re
+import itertools
 
 index_handlers = {
     "negate": -1,
@@ -33,11 +37,50 @@ index_handlers = {
 formats = ["#%", "+#%", "+#", "#"]
 
 
-import re
+re_number_pattern = r"\+?(-?\d+\.?\d*)\%?"
+re_insert_pattern = r"\+?-?{(\d+)}\%?"
+
+
+def _split_stat_translation(stat_translation):
+    """
+    takes in stat_translation and spits out 
+    string
+    sub_numbers
+    text_numbers
+    """
+    stat_splits = re.split(f"{re_insert_pattern}|{re_number_pattern}", stat_translation)
+    text_splits = stat_splits[::3]
+    sub_numbers = stat_splits[1::3]
+    text_numbers = stat_splits[2::3]
+    return text_splits, sub_numbers, text_numbers
+
+
+def _validate_stat_translation():
+    """
+
+    """
+
+    translation_strings = set()
+    for stat_translation in stat_translations:
+        if "English" in stat_translation:
+            english_stat_translation = stat_translation["English"]
+            for stat_translation_entry in english_stat_translation:
+                translation_strings.add(stat_translation_entry["string"])
+    split_strings = [_split_stat_translation(s) for s in translation_strings]
+    for s1, s2 in itertools.combinations(split_strings, 2):
+        text_splits_1, sub_numbers_1, text_numbers_1 = s1
+        text_splits_2, sub_numbers_2, text_numbers_2 = s2
+        if text_splits_1 == text_splits_2:
+            print(s1)
+            print(s2)
 
 
 def _return_possible_stats_from_explicit_line(explicit_line):
-    re_number_pattern = r"\+?(-?\d+\.?\d*)\%?"
+    """
+    takes in explicit line
+    first removes all numeric values (including +/-/%)
+    
+    """
 
     re_result = re.split(re_number_pattern, explicit_line)
     explicit_splits = re_result[::2]
@@ -47,34 +90,21 @@ def _return_possible_stats_from_explicit_line(explicit_line):
         stat_english = stat["English"]
 
         for stat_entry in stat_english:
-            stat_splits = re.split(
-                r"\+?-?{(\d+)}\%?|" + re_number_pattern, stat_entry["string"]
+            text_splits, sub_numbers, text_numbers = _split_stat_translation(
+                stat_entry["string"]
             )
-            text_splits = stat_splits[::3]
 
             if explicit_splits == text_splits:
-                sub_numbers = stat_splits[1::3]
-                text_numbers = stat_splits[2::3]
-                valid_entry = True
                 values = []
                 for sub_index, text_number, explicit_number in zip(
                     sub_numbers, text_numbers, explicit_numbers
                 ):
+                    # assert all numbers in pattern not in {} agree with query string
                     if text_number is not None:
                         if text_number != explicit_number:
-                            valid_entry = False
+                            break
                     else:
                         format_str = stat_entry["format"][int(sub_index)]
-
-                        # if "%" == format_str[-1]:
-                        #     if explicit_number[-1] != "%":
-                        #         valid_entry = False
-                        #     explicit_number = explicit_number[:-1]
-
-                        # if (explicit_number[0] not in ["+", "-"]) != (
-                        #     format_str[0] not in ["+", "-"]
-                        # ):
-                        #     valid_entry = False
 
                         value = float(explicit_number)
                         for index_handler in stat_entry["index_handlers"][
@@ -85,12 +115,12 @@ def _return_possible_stats_from_explicit_line(explicit_line):
 
                         conditions = stat_entry["condition"][int(sub_index)]
                         if "min" in conditions and conditions["min"] > value:
-                            valid_entry = False
+                            break
                         if "max" in conditions and conditions["max"] < value:
-                            valid_entry = False
+                            break
 
                         values.append(value)
-                if valid_entry:
+                else:
                     stat_entry = {}
                     for stat_id, value in zip(stat["ids"], values):
                         stat_entry[stat_id] = value
@@ -126,7 +156,7 @@ def _find_duplicate_stat_translations(stat_id_pool):
 
 if __name__ == "__main__":
     # test inputs
-
+    _validate_stat_translation()
     _return_possible_stats_from_explicit_line("+8 to Strength")
     _return_possible_stats_from_explicit_line("Regenerate 5.1 Mana per second")
     _return_possible_stats_from_explicit_line(
